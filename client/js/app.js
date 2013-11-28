9b8727b37add34d4f1f3693440665ebbcbb22e35
Alan Shaw <alan@freestyle-developments.co.uk>
2013-11-28 10:49:50 +0000

    Tutor profile page

--- client/js/app.js
+++ client/js/app.js
@@ -1,13 +1,13 @@
 App = {
-  
+
   api: '/api',
 
   geocoder: function () {
     return L.mapbox.geocoder('tutorwire.map-rbl1tiup')
   },
-  
+
   map: null,
-  
+
   initMap: function (id) {
     if (App.map) return App.map
 
@@ -23,7 +23,7 @@ App = {
 
     return map
   },
-  
+
   userMarker: null,
 
   showUserMarker: function(location) {
@@ -36,7 +36,7 @@ App = {
     })
 
     App.userMarker = L.marker(location.coords, {icon: icon, title: 'You are here'})
-    
+
     App.userMarker.addTo(App.map)
 
     App.map.setView(location.coords, 11)
@@ -46,13 +46,13 @@ App = {
     cb = cb || function () {}
 
     App.map.on('locationfound', function (event) {
-      
+
       App.geocoder().reverseQuery(event.latlng, function (err, response){
         if (err) { return console.error(err) }
-        
+
         // response looks like: {"query":[-0.0801,51.4657],"results":[[{"bounds":[-0.523222999999989,51.27866,0.336112,51.72023],"lat":51.5040006418191,"lon":-0.109467698133307,"name":"London","score":900001728809196.6,"type":"place","id":"mapbox-places.219827"},{"bounds":[-0.107894862857551,51.4191873235362,-0.0231455141541109,51.51158478481],"lat":51.4653860541731,"lon":-0.0636831061709452,"name":"Southwark","score":30926433.0294826,"type":"province","id":"province.2903"},{"bounds":[-13.6913559567794,49.9096161909876,1.77170536308596,60.8475532028857],"lat":54.3177967325959,"lon":-1.91064039912679,"name":"United Kingdom","population":61113205,"type":"country","id":"country.152"}]],"attribution":{"mapbox-places":"<a href='http://mapbox.com/about/maps' target='_blank'>Terms & Feedback</a>"}}
         var name = response.results[0][0].name
-        
+
         App.location = {
           name: name,
           coords: App.normalizeCoords(event.latlng)
@@ -73,17 +73,17 @@ App = {
     while (App.markers.length) {
       App.map.removeLayer(App.markers.pop())
     }
-  },  
+  },
 
   showTutorsOnMap: function (tutors) {
     App.clearMarkers()
 
     console.log('Showing tutors on map', tutors)
     for (var i = 0; i < tutors.length; i++) {
-      
+
       var tutor = tutors[i],
         icon = new L.Icon.Default
-      
+
       if (tutor.photo && tutor.photo.url) {
         icon = L.icon({
           className: 'photo-icon',
@@ -92,12 +92,12 @@ App = {
           popupAnchor: [4, -25]
         })
       }
-      
+
       var marker = L.marker(tutor.location.coords, {
         title: tutor.name,
         icon: icon
       })
-      
+
       marker.bindPopup(Template.profilePopup(tutor))
 
       marker.addTo(App.map)
@@ -116,15 +116,15 @@ App = {
   trimTo: function (number, decimalPlaces) {
     return parseFloat(number.toFixed(decimalPlaces))
   },
-  
+
   type: function (word, cb, done) {
     var index = 1
     var interval = setInterval(function () {
-      
+
       cb(word.substring(0, index))
-      
+
       index++
-      
+
       if (index > word.length) {
         clearInterval(interval)
         if ($.isFunction(done)){
@@ -134,7 +134,7 @@ App = {
     }, 100)
   },
 
-  mailto: function(opts){
+  mailto: function (opts) {
     return 'mailto:' + opts.email + "?subject=" + opts.subject.replace(/ /g, '%20') + '&body=' + opts.body.replace(/ /g, '%20')
   }
 }
--- client/js/tutor.js
+++ client/js/tutor.js
@@ -1,7 +1,35 @@
 TutorController = RouteController.extend({
   template: "tutor",
 
-  waitOn: function () {
-    return Meteor.subscribe("tutor-by-puid", this.params.puid)
+  before: function () {
+    Meteor.subscribe("tutor-by-puid", this.params.puid)
+  },
+
+  data: function () {
+    return Tutors.findOne()
+  },
+
+  unload: function () {
+    App.map.remove()
+    App.map = null
   }
 })
+
+Template.tutor.rendered = function () {
+  var map = App.initMap()
+    , tutor = Tutors.findOne()
+
+  if (!tutor) return;
+
+  $("#tutor-contact").attr("href", App.mailto({
+    email: tutor.email,
+    subject: "I'd like to learn " + tutor.subject,
+    body: "Hi, I'm looking to learn " + tutor.subject + " and I found your profile on tutorwire.com"
+  }))
+
+  var latlng = new L.LatLng(tutor.location.coords.lat, tutor.location.coords.lng)
+  console.log('coords', latlng)
+
+  new L.marker(latlng, {}).addTo(map)
+  map.setView(latlng, 8)
+}
