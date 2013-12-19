c2955ec95f0603867907c0a1efa0836c163139e9
olizilla <oli@zilla.org.uk>
2013-12-19 17:23:41 +0000

    Send a message to the recipient.

--- client/js/conversation.js
+++ client/js/conversation.js
@@ -2,13 +2,23 @@ ConversationController = RouteController.extend({
   template: "conversation",
 
   data: function () {
-      return {bodyClass: "conversation"}
+      return {
+        bodyClass: "conversation",
+        conversation: Conversations.findOne({puid: this.params.puid})
+      }
   }
 })
 
 Template.conversation.events = {
   'click button': function (evt, tpl) {
     var text = tpl.find('input').value
-    console.log('Send: ', text, this)
+    var fromId = Meteor.userId()
+    var users = this.conversation.users.filter(function(u){ return u._id != fromId})
+    var toId = users && users[0] && users[0].userId // Erk, make nice.
+    console.log('sending', text, toId, this)
+    Conversations.sendMessage(toId, text, function(er){
+      if (er) return console.error('Failed to send', er);
+      console.log('sent message')
+    })
   }
 }
