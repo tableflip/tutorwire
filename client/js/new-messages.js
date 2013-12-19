f86323874a56939c4298da4eef00388ac28d8bbd
olizilla <oli@zilla.org.uk>
2013-12-19 16:56:07 +0000

    Rename conversations to new-messages

--- client/js/new-messages.js
+++ client/js/new-messages.js
@@ -1,10 +1,10 @@
-Template.conversations.new = function (userId) {
+Template.newMessages.conversations = function (userId) {
   // TODO: subscription should filter to new ones
   return Conversations.find({})
 }
 
 // Get the other user in this conversation
-Template.conversations.otherUser = function (users) {
+Template.newMessages.otherUser = function (users) {
   var others = users.filter(function (user) {
     return user._id !== Meteor.userId()
   })
@@ -12,7 +12,7 @@ Template.conversations.otherUser = function (users) {
   return others[0] // TODO: First user that is not me... will have to pick the last message and find the user for that for multi chat.
 }
 
-Template.conversations.events = {
+Template.newMessages.events = {
   'click a': function (evt) {
     // `this` is the conversation object
     Router.go('conversation', {puid: this.puid})
