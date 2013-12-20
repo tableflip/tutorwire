8fad057d89d08e2cf43897326c126152891ef5fe
Alan Shaw <alan@freestyle-developments.co.uk>
2013-12-20 10:41:20 +0000

    Further conversation styles

--- client/js/conversation.js
+++ client/js/conversation.js
@@ -5,3 +5,18 @@ ConversationController = RouteController.extend({
       return {bodyClass: "conversation"}
   }
 })
+
+function resizeMessagesWindow () {
+    var winHeight = $(window).height()
+    $("#messages").height(winHeight - 180)
+}
+
+function scrollBottomMessagesWindow () {
+    var msgs = $("#messages")
+    msgs.animate({scrollTop: msgs.height()}, 500)
+}
+
+Template.conversation.rendered = function () {
+    resizeMessagesWindow()
+    scrollBottomMessagesWindow()
+}
