bde101a227c7d2c7106029098c959f3ac1631678
olizilla <oli@zilla.org.uk>
2013-12-20 14:35:53 +0000

    Fix conversation bug.

--- client/js/conversation.js
+++ client/js/conversation.js
@@ -66,7 +66,7 @@ Template.conversation.events = {
     var fromId = Meteor.userId()
 
     // TODO: push this into sendMessage, we shouldn't have to unpick the other recipients each time.
-    var users = this.conversation.users.filter(function(u){ return u._id != fromId})
+    var users = this.conversation.users.filter(function(u){ return u.userId != fromId})
     var toId = users && users[0] && users[0].userId // Erk, make nice.
     console.log('sending', text, toId, this)
     Conversations.sendMessage(toId, text, function(er){
